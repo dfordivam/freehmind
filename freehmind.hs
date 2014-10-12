f0603e1fed0f46b412cb6f9baa0a385bf5657552
divam <divam@localhost.localdomain>
2014-10-12 11:57:55 -0400

    rendering child nodes, adjacent

--- freehmind.hs
+++ freehmind.hs
@@ -7,6 +7,7 @@ import "gtk" Graphics.UI.Gtk
 import Diagrams.Backend.Cairo
 import Diagrams.Prelude
 import Diagrams.Backend.Gtk
+import Diagrams.TwoD.Size
 
 main :: IO ()
 main = do
@@ -39,7 +40,9 @@ figure =  unitCircle # scaleX 0.5 # rotateBy (1/6) # scale 50 # fc red
 data FhmNode = FhmNode String [ FhmNode ]
 
 sampleMM :: FhmNode
-sampleMM = FhmNode "Root" []
+sampleMM = FhmNode "Root" [(leafN "hello"), (leafN "world")]
+
+leafN txt = FhmNode txt []
 
 -- Rendering Algorithm
 -- The root node is kept at the middle and its children nodes are rendered
@@ -59,7 +62,20 @@ horizontalSize node = sum (mySize: (map horizontalSize children) )
     where mySize   = 50
           children = (\(FhmNode _ x) -> x) node
 
-
 renderMindMap :: FhmNode -> Diagram Cairo R2
-renderMindMap node = text value # fontSizeN 0.2 <> square 1 # scale 50
+renderMindMap root = (renderNode root) ||| (renderChildren children)
+    where children = (\(FhmNode _ x) -> x) root
+
+renderNode :: FhmNode -> Diagram Cairo R2
+renderNode node = text txt # fontSizeN 0.1 <> square 1 # scale 50
     where value = (\(FhmNode x _) -> x) node
+          size  = Diagrams.TwoD.Size.size2D emptyNode
+          txt     = value
+
+emptyNode :: Diagram Cairo R2
+emptyNode = square 1 # scale 50
+
+renderChildren :: [FhmNode] -> Diagram Cairo R2
+renderChildren [] = circle 1
+renderChildren (x:xs) = renderNode x === renderChildren xs
+
